f66b3a476b76c5a19b3ee52009bf5f370299620a
Graham Wheeler <gram@geekraver.com>
2025-02-07 16:24:06 -0800

    Fix click options

--- dedupper/__init__.py
+++ dedupper/__init__.py
@@ -10,8 +10,8 @@ from .dedupper import dedup
 @click.command()
 @click.option('-t', '--token', help='GitHub PAT with issue read/write rights for the repo, if not from environment.', default='--')
 @click.option('-d', '--database', help='Database file for persistent state.', default='')
-@click.option('-c', '--close', type=bool, help='Close any duplicates found.', default=False)
-@click.option('-p', '--pretend', type=bool, help='Do a dry run without making any changes.', default=False)
+@click.option('--close/--no-close', type=bool, help='Close any duplicates found.', default=False)
+@click.option('--pretend/--no-pretend', type=bool, help='Do a dry run without making any changes.', default=False)
 @click.argument('repo', required=1)
 def dedupper(token, database, close, pretend, repo):
     """ REPO: Repository name in form org/repo. """    
